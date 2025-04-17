14afc3245688de40c5686c464bcc7b501c6e60a0
kwon <galaxys7alden@gmail.com>
2025-04-17 21:41:45 +0800

    ESLint 완전 무시 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
